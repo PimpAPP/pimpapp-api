2939d2162bc486bebe1a5927d0bcba88e574fe66
viniciusarcanjo <arcanjov8@gmail.com>
2015-11-14 11:22:10 -0200

    implementado token authentication e alguns novos filtros; atualizado readme e documentacao tambem

--- app_site/carroceiro/views.py
+++ app_site/carroceiro/views.py
@@ -5,16 +5,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
 from rest_framework import status
+from rest_framework import filters
+from rest_framework import generics
+from geopy.distance import vincenty
 
 
-class CarroceirosList(APIView):
+class CarroceirosList(generics.ListAPIView):
     """
     List all Carroceiros and/or Create a new Carroceiro.
+    This view allows filtering by the attributes "type" and "phone"
     """
-    def get(self, request, format=None):
-        carroceiros = Carroceiro.objects.all()
-        serializer = CarroceiroSerializer(carroceiros, many=True)
-        return Response(serializer.data)
+    queryset = Carroceiro.objects.all()
+    serializer_class = CarroceiroSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = ('type', 'phone')
 
     def post(self, request, format=None):
         serializer = CarroceiroSerializer(data=request.data)
@@ -27,22 +31,31 @@ class CarroceirosList(APIView):
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CarroceiroFindByPhone(APIView):
+class CarroceiroRadiusFilter(APIView):
     """
-    List all Carroceiros given their cellphone.
-    It's assumed that a Carroceiro can have multiple addresses, hence one cellphone would have multiple entries .
+    List all Carroceiros who are within a certain radius in kilometers given the user's latitude and longitude which
+    is the reference point to calculte this radius filter.
     """
-    def get(self, request, phone, format=None):
+    def get(self, request, lat_1, long_1, radius, format=None):
         try:
-            carroceiro_list = Carroceiro.objects.filter(phone=phone)
-            carroceiro_list_serializer = CarroceiroSerializer(carroceiro_list, many=True)
+            carroceiro_full_list = Carroceiro.objects.all()
+            carroceiro_list_serializer = CarroceiroSerializer(self.filterWithinRadius(carroceiro_full_list, lat_1, long_1, radius), many=True)
             return Response(carroceiro_list_serializer.data)
         except Carroceiro.DoesNotExist:
             raise Http404
 
+    def filterWithinRadius(self, carroceiro_list, lat1, long1, radius):
+        filter_carroceiro_list = []
+        for c in carroceiro_list:
+            (lat_aux, long_aux) = (c.latitude, c.longitude)
+            if vincenty((lat_aux, long_aux), (lat1, long1)).km <= float(radius):
+                filter_carroceiro_list.append(c)
+        return filter_carroceiro_list
+
+
 class CarroceiroDetail(APIView):
     """
-    Update ,Retrieve and Delete a instance of Carroceiro given their id.
+    Update, Retrieve and Delete a instance of Carroceiro given their id.
     """
     def get(self, request, id, format=None):
         try:
--- integration_tests/curl_shell_cmds.md
+++ integration_tests/curl_shell_cmds.md
@@ -20,5 +20,14 @@ curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X PUT
 
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X DELETE -d '{"name":"test","phone":"22222222222","address":"Av Independencia, 500","latitude":-11.111111,"longitude":-22.222222}' http://127.0.0.1:8000/carroceiro/20/
 
+#############################
+# curl Token authentication #
+#############################
 
+right token:
 
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token 2ad7cd83134a4908f8277c058a7b7da23b2876bc" -X POST -d '{"name": "carroceiro_test", "type": "carroceiro", "phone": "999111111", "address": "Av test, 9999", "latitude": -11.11, "longitude": -22.22}' http://127.0.0.1:8000/carroceiro/
+
+wrong token: 401 return..
+
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token 3ad7cd83134a4908f8277c058a7b7da23b2876bc" -X POST -d '{"name": "carroceiro_test", "type": "carroceiro", "phone": "999111111", "address": "Av test, 9999", "latitude": -11.11, "longitude": -22.22}' http://127.0.0.1:8000/carroceiro/
