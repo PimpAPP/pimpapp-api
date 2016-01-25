2176626955f1095183afc30e0930a8e34225c153
Vido <vido@usp.br>
2016-01-25 15:53:37 -0200

    Photos, Rating and Profile views

--- app_site/api/serializers.py
+++ app_site/api/serializers.py
@@ -1,22 +1,28 @@
 from rest_framework import serializers
 
-from views.models import ProfileInfo
-from views.models import Rating
-from views.models import Photo
+from .models import ProfileInfo
+from .models import Rating
+from .models import Photo
 
 class ProfileInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProfileInfo
-        fields = ('pk', 'name', 'mno', 'has_whatsapp', 'address', 'region',
+        fields = ('pk',
+            'user', 'carroceiro', 'created_on', 'moderation_status'
+            'name', 'mno', 'has_whatsapp', 'address', 'region',
             'address', 'region', 'city', 'has_motor_vehicle',
             'carroca_pimpada')
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
-        fields = ('rating', 'comment')
+        fields = ('pk',
+            'user', 'carroceiro', 'created_on', 'moderation_status'
+            'rating', 'comment')
 
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
-        fields = ('full_photo', 'thumbnail')
+        fields = ('pk',
+            'user', 'carroceiro', 'created_on', 'moderation_status'
+            'full_photo', 'thumbnail')
--- app_site/api/views.py
+++ app_site/api/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import ProfileInfo
+from .models import Rating
+from .models import Photo
+
+#from .models.Authorship import APPROVED
+#from .models.Authorship import PENDING
+from .models import Authorship
+
+from .serializers import ProfileInfoSerializer
+from .serializers import RatingSerializer
+from .serializers import PhotoSerializer
+
+public_status = (Authorship.APPROVED, Authorship.PENDING)
+
+class ProfileInfoViewSet(viewsets.ModelViewSet):
+    """
+        DOCS: TODO
+    """
+    serializer_class = ProfileInfoSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    lookup_field = 'carroceiro'
+    queryset = ProfileInfo.objects.filter(
+            moderation_status__in=public_status)
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    """
+        DOCS: TODO
+    """
+    serializer_class = RatingSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    lookup_field = 'carroceiro'
+    queryset = Rating.objects.filter(
+            moderation_status__in=public_status)
+
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    """
+        DOCS: TODO
+    """
+    serializer_class = PhotoSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    lookup_field = 'carroceiro'
+    queryset = Photo.objects.filter(
+            moderation_status__in=public_status)
