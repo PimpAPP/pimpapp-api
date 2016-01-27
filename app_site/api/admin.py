881e0f64ee3fa669cde04294953311b70b592f9b
Vido <vido@usp.br>
2016-01-27 11:21:13 -0200

    Setting the Admin

--- app_site/api/admin.py
+++ app_site/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Carroceiro
+from .models import Materials
+from .models import LatitudeLongitude
+from .models import Rating
+from .models import Photo
+from .models import ProfileInfo
+from .models import ProfileInfoHistoric
+
+admin.site.register(Carroceiro)
+admin.site.register(Materials)
+admin.site.register(LatitudeLongitude)
+admin.site.register(Rating)
+admin.site.register(Photo)
+admin.site.register(ProfileInfo)
+admin.site.register(ProfileInfoHistoric)
