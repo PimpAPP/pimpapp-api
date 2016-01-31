0da93a88392fffe01e24d515ee7e0dc30cd451f0
Vido <vido@usp.br>
2016-01-30 22:42:03 -0200

    Setting media root

--- app_site/app_site/urls.py
+++ app_site/app_site/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls')),
     #url(r'^api/carroceiro', include('carroceiro.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
